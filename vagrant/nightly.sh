5f7560f35a6c99a542d121564a78ef2659ef1349
Warren Strange <warren.strange@gmail.com>
2015-10-02 18:25:38 -0600

    changed yum to dnf

--- vagrant/nightly.sh
+++ vagrant/nightly.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Get the nightly builds using maven
-# This is an alternative to using the getnightly.sh shell script
+# This is an alternative to using the ../bin/getnightly.sh shell script
 
 rm -fr ./staging
 mkdir staging
