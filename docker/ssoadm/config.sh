871ca4c8354d5b7cc05f927d1f1dc9be8551aa75
Warren Strange <warren.strange@forgerock.com>
2015-11-11 15:41:00 -0700

    added openam build

--- docker/ssoadm/config.sh
+++ docker/ssoadm/config.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
+# TODO: We need some process that sleeps until AM is up, then configures ssoadm, then applies configuration
 
-cd /var/tmp
+cd /opt/ssoadm
 
-C=openam-configurator-tool*.jar
-
-# todo: Put a wait loop for the openam-svc-a:80 to come up before running
-
-echo configuring first instance
-
-java -jar $C -f master.properties
-
-sleep 30
-echo Configuring second instance
-
-java -jar $C -f second.properties
-
-echo done config
 
 # todo - take this out when we get it right. This is to allow us to log on to the container.
-sleep 1000
+while true; do sleep 1000; done
