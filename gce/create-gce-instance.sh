819b10fc6ce2e609659d65b2899ca9eb1fdb3a79
Warren Strange <warren.strange@gmail.com>
2015-12-11 15:40:42 -0400

    updates for ingress testing

--- gce/create-gce-instance.sh
+++ gce/create-gce-instance.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # Sample command to create a GCE instance for running frstack
 
-gcloud compute --project "frstack-1077" instances create "instance-1" --zone "us-central1-f"  \
-   --machine-type "n1-standard-2" --network "default" --no-restart-on-failure --maintenance-policy "TERMINATE"
-   --preemptible --scopes "https://www.googleapis.com/auth/cloud-platform" \
-   --tags "http-server" "https-server" "frstack" \
-    --image "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20150915" \
-    --boot-disk-size "15" --boot-disk-type "pd-standard"  \
-    --boot-disk-device-name "instance-1"
-
+gcloud compute --project "forgerockdemo" instances create "frstack" \ 
+--zone "us-central1-f" --machine-type "n1-standard-2" --network "default" \
+--no-restart-on-failure --maintenance-policy "TERMINATE" --preemptible --scopes \ 
+"https://www.googleapis.com/auth/cloud.useraccounts.readonly", \
+"https://www.googleapis.com/auth/devstorage.read_only", \
+"https://www.googleapis.com/auth/logging.write", \
+"https://www.googleapis.com/auth/monitoring.write" \
+--tags "http-server" "https-server" \
+--image  "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20151104" \
+--boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "frstack"
