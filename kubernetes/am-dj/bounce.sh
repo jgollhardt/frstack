d95565c22207166049fb54fe65eb8c22c7e66d3d
Warren Strange <warren.strange@gmail.com>
2016-01-15 17:56:46 -0700

    added OpenIDM,

--- kubernetes/am-dj/bounce.sh
+++ kubernetes/am-dj/bounce.sh
@@ -2,17 +2,20 @@
 # Bounce all except service (so we dont lose our IP). Used for testing
 
 
-SVC="amconfig openam-controller opendj-controller ssoconfig"
+SVC="opendj-controller openam-controller ssoconfig"
 
 for svc in $SVC
 do
   kubectl delete -f $svc.yaml
 done
 
-sleep 10
+echo waiting...
+sleep 30
 
 
 for svc in $SVC
 do
+ echo $svc
   kubectl create -f $svc.yaml
+  sleep 10
 done
